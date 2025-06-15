f714936b511b73b4f8d902d991f3aa83bee1c728
jacques <jlefort251@gmail.com>
2025-06-15 15:12:05 +0200

    ha ha

--- prof6.py
+++ prof6.py
@@ -4,12 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello Hello"
-    
-    
+@app.route('/test/<xxx>')
+def check_value(xxx):
+    if xxx == "hello":
+        return "helli"
+    else:
+        return f"Tu as envoyé : {xxx}"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render impose un port
     app.run(host="0.0.0.0", port=port)
+
+
+
