61784ce771a416b9cb7332bb060f1215f728d814
jacques <jlefort251@gmail.com>
2025-06-16 11:48:09 +0200

    cocorico

--- prof6.py
+++ prof6.py
@@ -1,15 +1,12 @@
 
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello Hello hello"
-    
-    
-    
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render impose un port
