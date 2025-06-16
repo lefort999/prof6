edf69a84b4c318d209d7f73a44422207268cfe91
jacques <jlefort251@gmail.com>
2025-06-16 16:04:01 +0200

    hum hum

--- prof6.py
+++ prof6.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def home():
+def index():
     message = ""
     if request.method == "POST":
         profession = request.form.get("profession")
-        if profession.lower() == "douanier":
-            message = "Si profession = douanier, leurs archives sont aux archives nationales."
+        message = f"Votre profession est : {profession}"
     return render_template("index.html", message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Render attribue automatiquement un port
+    app.run(host="0.0.0.0", port=port)
