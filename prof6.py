08b7a5e26b0f67042b1dcf50db51cff7fe9742cc
jacques <jlefort251@gmail.com>
2025-06-17 16:25:36 +0200

    chance

--- prof6.py
+++ prof6.py
@@ -31,4 +31,4 @@ def profession():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Render attribue automatiquement un port
     app.run(host="0.0.0.0", port=port)
-Explications :
+
