2773d7683b86a445ee042630cffc263b9f33aad7
jacques <jlefort251@gmail.com>
2025-06-18 12:46:02 +0200

    Ajout des fichiers prof20 et prof30

---
+++ prof30.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, world!'
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render attribue automatiquement un port
+    app.run(host="0.0.0.0", port=port)
+
