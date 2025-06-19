051e935892b54fd183e0b2e6db4d6eec53fde07f
jacques <jlefort251@gmail.com>
2025-06-19 16:31:22 +0200

    eureka

--- prof6.py
+++ prof6.py
@@ -1,12 +1,32 @@
 import os
 from flask import Flask, request, render_template
 
-
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, world!'
+def lire_texte(nom_fichier):
+    """Lit le contenu d'un fichier texte."""
+    try:
+        with open(nom_fichier, "r", encoding="utf-8") as fichier:
+            return fichier.read()
+    except FileNotFoundError:
+        return "Information non disponible."
+
+@app.route("/")
+def recherche():
+    return render_template("index.html")
+
+@app.route("/profession", methods=["GET", "POST"])
+def profession():
+    message = ""
+    if request.method == "POST":
+        profession = request.form.get("profession").lower()
+        if profession == "militaire":
+            message = f"cette rubrique concerne le : {profession}. {lire_texte('militaire.txt')}"
+        elif profession == "fisc":
+            message = f"cette rubrique concerne le : {profession}. {lire_texte('fisc.txt')}"
+        else:
+            message = f"cette rubrique concerne le : {profession}."
+    return render_template("index.html", message=message)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Render attribue automatiquement un port
